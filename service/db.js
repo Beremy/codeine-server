3749b18b43f794c8f80b89d8a71ff67ad5293ef6
hostomytho <bertrand.remy@inria.fr>
2023-04-13 13:31:24 +0200

    Connect config width .env

--- service/db.js
+++ service/db.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("hostomytho_bdd", "root", "", {
-  dialect: "mysql",
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
 });
 
 const connectToDb = async () => {
