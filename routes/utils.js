627da9572913b969e49a5a4f087229254dd2e075
hostomytho <bertrand.remy@inria.fr>
2023-07-04 16:55:00 +0200

    Create function sendMail

--- routes/utils.js
+++ routes/utils.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const { MessageMenu } = require("../models");
 const authMiddleware = require("../middleware/authMiddleware");
+const nodemailer = require("nodemailer");
 
 router.get("/protected-route", authMiddleware, (req, res) => {
   // Route protégée par l'authentification
@@ -9,7 +10,7 @@ router.get("/protected-route", authMiddleware, (req, res) => {
 
 router.get("/messageMenu", async function (req, res, next) {
   try {
-    const messageMenu = await MessageMenu.findOne({ where: { active: true }});
+    const messageMenu = await MessageMenu.findOne({ where: { active: true } });
     res.json(messageMenu);
   } catch (err) {
     next(err);
@@ -26,7 +27,7 @@ router.put("/messageMenu", async function (req, res, next) {
       await existingMessageMenu.save();
       res.json(existingMessageMenu);
     } else {
-      res.status(404).send('Message not found');
+      res.status(404).send("Message not found");
     }
   } catch (err) {
     next(err);
@@ -41,5 +42,37 @@ router.put("/messageMenu", async function (req, res, next) {
 //     next(err);
 //   }
 // });
+
+router.post("/sendMail", async function (req, res, next) {
+  console.log("sendMail");
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: "gmail", //email provider
+    auth: {
+      user: "your-email@gmail.com", 
+      pass: "your-password",
+    },
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Your Name" <your-email@gmail.com>', // sender address
+    to: req.body.mail, // list of receivers
+    subject: "Password Recovery", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ error: "Error while sending mail" });
+    } else {
+      console.log("Message sent: %s", info.messageId);
+      res.status(200).json({ message: "Mail successfully sent" });
+    }
+  });
+});
 
 module.exports = router;
