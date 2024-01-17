fe7a94738d71b050321e45b37531536602936fc5
hostomytho <bertrand.remy@inria.fr>
2024-01-17 11:34:32 +0100

    Change log month_end.js

--- scripts/month_end.js
+++ scripts/month_end.js
@@ -19,11 +19,7 @@ const sequelize = new Sequelize(
 );
 
 const logToFile = (message) => {
-  // const logFile = path.join(__dirname, "month_end.log");
-  // const frTime = new Date().toLocaleString("fr-FR", {
-  //   timeZone: "Europe/Paris",
-  // });
-  // fs.appendFileSync(logFile, `${frTime} - ${message}\n`);
+  console.log(message);
 };
 
 const resetMonthlyPoints = async () => {
