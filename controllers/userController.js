d50b77d2ec5124e64825b2554d2be9a2a0be5a2e
hostomytho <bertrand.remy@inria.fr>
2024-04-30 11:26:11 +0200

    Email already used

--- controllers/userController.js
+++ controllers/userController.js
@@ -82,12 +82,18 @@ const createUser = async (user) => {
   }
 };
 
-const getUserByUsername = async (username) => {
-  return await User.findOne({ where: { username } });
-};
 
 const signup = async (req, res) => {
   try {
+    const existingUserByEmail = await User.findOne({
+      where: { email: req.body.email }
+    });
+
+    if (existingUserByEmail) {
+      return res.status(409).json({ error: "Cette adresse email est déjà utilisée." });
+    }
+
+    // Création de l'utilisateur
     const user = await createUser(req.body);
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
       expiresIn: "24h",
@@ -106,30 +112,43 @@ const signup = async (req, res) => {
   }
 };
 
+
+
+const getUserByUsername = async (username) => {
+  return await User.findOne({ where: { username } });
+};
+
+const getUserByEmail = async (email) => {
+  return await User.findOne({
+    where: { email: email }
+  });
+};
+
+
+async function getUserByUsernameOrEmail(identifier) {
+  let user = await getUserByUsername(identifier);
+  if (user) return user;
+  user = await getUserByEmail(identifier);
+  return user;
+}
+
 const signin = async (req, res) => {
   try {
-    const user = await getUserByUsername(req.body.username);
+    const user = await getUserByUsernameOrEmail(req.body.username);
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
 
-    const isPasswordValid = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
+    const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
     if (!isPasswordValid) {
       return res.status(401).json({ error: "Invalid password" });
     }
 
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "24h",
-    });
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "24h" });
 
     const userInfo = user.get({ plain: true });
     delete userInfo.password;
-    res
-      .status(200)
-      .json({ message: "User signed in successfully", token, user: userInfo });
+    res.status(200).json({ message: "User signed in successfully", token, user: userInfo });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -724,6 +743,7 @@ module.exports = {
   signin,
   getAllUsers,
   getUserById,
+  getUserByEmail,
   getUsersOrderedByPoints,
   getUsersOrderedByPointsInMonthly,
   getUserRanking,
