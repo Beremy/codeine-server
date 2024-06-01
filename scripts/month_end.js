74a48a0830cb3dc931fc654cff9fb22820f31ff9
hostomytho <bertrand.remy@inria.fr>
2024-06-01 17:43:07 +0200

    Fix script month_end

--- scripts/month_end.js
+++ scripts/month_end.js
@@ -1,4 +1,8 @@
-const { Sequelize, DataTypes, QueryTypes } = require("sequelize");
+
+const { Sequelize, QueryTypes } = require("sequelize");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
